cce3c88d8fb3a250ca672b4032e585a6fceb664f
Alex-Domini <alexandr.s.f87@gmail.com>
2022-04-11 23:11:03 +0600

    added footer, added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-  
+  $('.slider').slick({
+    dots: false,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="arrow-right"></button>',
+  });
 });
