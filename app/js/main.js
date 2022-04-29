7a14f8d4a13ed1a463017af23d9986b2e86d3b8a
Alex-Domini <alexandr.s.f87@gmail.com>
2022-04-29 14:03:57 +0600

    add mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,15 @@ $(function () {
     nextArrow: $(".top-slider__btn-next"),
   });
 
-  var mixer = mixitup(".top-products__cards");
-  var mixer = mixitup(".sales-products__cards");
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: "local",
+    },
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 });
