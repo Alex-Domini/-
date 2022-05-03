d127e8459b54bc47f7c53ea749407cf8adbfca5b
Alex-Domini <alexandr.s.f87@gmail.com>
2022-05-03 17:29:37 +0600

    add slider brands

--- app/js/main.js
+++ app/js/main.js
@@ -16,4 +16,14 @@ $(function () {
 
   var mixer1 = mixitup(containerEl1, config);
   var mixer2 = mixitup(containerEl2, config);
+
+  $(".brands-slider__inner").slick({
+    arrows: false,
+    dots: false,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 });
